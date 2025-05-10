ee158031a21bb5bfa3679823ccf9dffac2b571f7
SAKTHIVEL675 <velparamesh75@gmail.com>
2025-05-10 08:37:41 +0530

    Update config.js

--- js/config.js
+++ js/config.js
@@ -1,7 +1,7 @@
 window._config = {
     cognito: {
-        userPoolId: 'ap-south-1_m4hfAHIdR', // e.g. us-east-2_uXboG5pAb
-        userPoolClientId: '1pt753mk5uorne6fp9ajeu90gl', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
+        userPoolId: 'ap-south-1_ngGb2DX0V', // e.g. us-east-2_uXboG5pAb
+        userPoolClientId: '14ubhm9gfmhv6fid3b9g04mebn', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
         region: 'ap-south-1' // e.g. us-east-2
     },
     api: {
